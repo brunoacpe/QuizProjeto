11d0ab436ac09cc63549b2c08f5f0348993a2632
VitóriaBentes <vitoriabentes04@gmail.com>
2021-07-17 12:05:09 -0300

    Arrumando o método POST, implementando novos métodos e inserindo TODO na classe QuizController

--- src/main/java/br/com/letscode/Controllers/QuizController.java
+++ src/main/java/br/com/letscode/Controllers/QuizController.java
@@ -5,6 +5,7 @@ import br.com.letscode.DAO.MovieDAO;
 import br.com.letscode.DAO.UsuarioDAO;
 import br.com.letscode.Model.Movie;
 import br.com.letscode.Model.Usuario;
+import br.com.letscode.Services.UsuarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,11 +25,13 @@ public class QuizController {
 
     private MovieDAO movieDAO;
     private UsuarioDAO usuarioDAO;
+    private UsuarioServices usuarioServices;
 
     @Autowired
-    public QuizController(MovieDAO movieDAO, UsuarioDAO usuarioDAO){
+    public QuizController(MovieDAO movieDAO, UsuarioDAO usuarioDAO, UsuarioServices usuarioServices){
         this.movieDAO = movieDAO;
         this.usuarioDAO = usuarioDAO;
+        this.usuarioServices = usuarioServices;
     }
 
 
@@ -39,20 +42,28 @@ public class QuizController {
     }
 
     //Post -> enviar requisição usuário, senha e id do filme/série vencedor (Retornar True ou false);
+    //TODO Criar exceção de senha/usuário inválido e de ImdbId do filme inválido
     @PostMapping
     public boolean verificarResultado(@PathVariable String opcaoSelecionada, @RequestBody Usuario usuario){
         Optional<Usuario> jogador = usuarioDAO.listarTodos().stream().filter(u -> u.equals(usuario)).findFirst();
         List<Movie> opcoesDoQuiz = listarFilmes();
         Collections.sort(opcoesDoQuiz,  Comparator.comparing(Movie::getScore));
+        Optional<Movie> opcaoSelecionadaValida = opcoesDoQuiz.stream().filter(m -> m.getImdbId().equals(opcaoSelecionada)).findFirst();
         boolean result = false;
-        if(jogador.isPresent()){
+        if(jogador.isPresent() && opcaoSelecionadaValida.isPresent()){
             if(opcoesDoQuiz.get(0).getImdbId().equals(opcaoSelecionada)){
                 result = true;
+                this.atualizarPontos(usuario, result);
             }else{
                 result = false;
+                this.atualizarPontos(usuario, result);
             }
         }
         return result;
+    }
+
+    public void atualizarPontos(Usuario usuario, boolean result){
+        //TODO colocar o método de UsuarioSerives aqui !
     }
 
 
