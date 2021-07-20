d54e33aaab8a64bc989c0094bfcfecdb0128f264
vitoriabentes <vitoriabentes04@gmail.com>
2021-07-20 12:10:55 -0300

    Consertando erros de código

--- src/main/java/br/com/letscode/Controllers/QuizController.java
+++ src/main/java/br/com/letscode/Controllers/QuizController.java
@@ -3,8 +3,13 @@ package br.com.letscode.Controllers;
 
 import br.com.letscode.DAO.MovieDAO;
 import br.com.letscode.DAO.UsuarioDAO;
+import br.com.letscode.Exceptions.UsuarioNaoEncontrado;
+import br.com.letscode.Exceptions.VidaInsuficiente;
 import br.com.letscode.Model.Movie;
 import br.com.letscode.Model.Usuario;
+import br.com.letscode.Services.JogoServices;
+import br.com.letscode.Services.MovieServices;
+import br.com.letscode.Services.QuizzServices;
 import br.com.letscode.Services.UsuarioServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +17,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -23,49 +30,60 @@ import java.util.Optional;
 @RestController
 public class QuizController {
 
-    private MovieDAO movieDAO;
-    private UsuarioDAO usuarioDAO;
     private UsuarioServices usuarioServices;
+    private MovieServices movieServices;
+    private QuizzServices quizzServices;
+    private JogoServices jogoServices;
 
     @Autowired
-    public QuizController(MovieDAO movieDAO, UsuarioDAO usuarioDAO, UsuarioServices usuarioServices){
-        this.movieDAO = movieDAO;
-        this.usuarioDAO = usuarioDAO;
+    public QuizController(UsuarioServices usuarioServices, MovieServices movieServices,QuizzServices quizzServices,JogoServices jogoServices){
         this.usuarioServices = usuarioServices;
+        this.movieServices = movieServices;
+        this.quizzServices = quizzServices;
+        this.jogoServices = jogoServices;
     }
 
 
     @GetMapping
     public List<Movie> listarFilmes(){
-        //No caso só retorna 2 filmes;
-        return movieDAO.listarTodos();
+        return movieServices.filmesAleatorios();
     }
 
-    //Post -> enviar requisição usuário, senha e id do filme/série vencedor (Retornar True ou false);
-    //TODO Criar exceção de senha/usuário inválido e de ImdbId do filme inválido
     @PostMapping
-    public boolean verificarResultado(@PathVariable String opcaoSelecionada, @RequestBody Usuario usuario){
-        Optional<Usuario> jogador = usuarioDAO.listarTodos().stream().filter(u -> u.equals(usuario)).findFirst();
-        List<Movie> opcoesDoQuiz = listarFilmes();
+    public String verificarResultado(@RequestParam("opcaoSelecionada") String opcaoSelecionada, @RequestBody Usuario usuario) throws UsuarioNaoEncontrado, VidaInsuficiente, IOException {
+        Usuario usuarioJogador = new Usuario();
+        usuarioJogador.setNome(usuario.getNome());
+        usuarioJogador.setSenha(usuario.getSenha());
+        Optional<Usuario> jogador = usuarioServices.procurarUsuario(usuarioJogador);
+        List<Movie> opcoesDoQuiz = movieServices.filmesAleatoriosQuiz();
         Collections.sort(opcoesDoQuiz,  Comparator.comparing(Movie::getScore));
-        Optional<Movie> opcaoSelecionadaValida = opcoesDoQuiz.stream().filter(m -> m.getImdbId().equals(opcaoSelecionada)).findFirst();
+        Optional<Movie> opcaoSelecionadaValida = opcoesDoQuiz.stream()
+                .filter(m -> m.getImdbId().equals(opcaoSelecionada))
+                .findFirst();
         boolean result = false;
         if(jogador.isPresent() && opcaoSelecionadaValida.isPresent()){
-            if(opcoesDoQuiz.get(0).getImdbId().equals(opcaoSelecionada)){
+            if(opcoesDoQuiz.get(1).getImdbId().equals(opcaoSelecionada)){
                 result = true;
-                this.atualizarPontos(usuario, result);
+                jogador.get().setCombo(jogador.get().getCombo()+0.1);
+                usuarioServices.atualizarPontos(usuario, quizzServices.pontosFeitos(jogador.get()));
             }else{
                 result = false;
-                this.atualizarPontos(usuario, result);
+                jogador.get().setCombo(1);
             }
         }
-        return result;
+        String resultado;
+        if(result){
+            resultado = "TRUE";
+        }else {
+            resultado = "FALSE";
+        }
+        usuarioServices.vidas(jogador, result);
+        jogoServices.atualizarJogo(jogador);
+        return resultado;
     }
 
-    public void atualizarPontos(Usuario usuario, boolean result){
-        //TODO colocar o método de UsuarioSerives aqui !
-    }
 
 
 
 }
+
